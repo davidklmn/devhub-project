d31c130d016e3b51a6300fbee777cd3cb4fe0aee
GAK001 <gak.ahmetkaraca@gmail.com>
2023-03-17 18:51:38 +0000

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-import JSearchAPI from './components/utils/JSearchAPI';
-import JobSearch from './components/JobSearch.js';
+import "./App.css";
+import Navigation from "./components/Navigation";
+//import JSearchAPI from "./components/utils/JSearchAPI";
+//import JobSearch from "./components/JobSearch.js";
+import ".././node_modules/uikit/dist/css/uikit.min.css";
+import ".././node_modules/uikit/dist/js/uikit.min.js";
+import ".././node_modules/uikit/dist/js/uikit-icons.min.js";
 
 function App() {
-  return <JobSearch></JobSearch>;
+  return (
+    <div>
+      <Navigation />;
+    </div>
+  );
 }
 
 export default App;
