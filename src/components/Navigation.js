f9b3cd55b50a68a5181107da3db917a4e5214d46
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-03-17 19:58:00 +0000

    navigation page/adding router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Articles from "../components/pages/Articles"
+import Home from "../components/pages/Home"
+import Jobs from "../components/pages/Jobs"
+import Saved from "../components/pages/Saved"
 import {
   Nav,
   Container,
@@ -13,26 +17,35 @@ import {
   NavbarContainer,
   NavbarSticky,
   Section,
-  NavItem,
+
 } from "uikit-react";
 
 function Navigation() {
   return (
-    <Nav preset="default">
-      {" "}
-      <NavItem>
-        {" "}
-        <Link href="#">Link</Link>{" "}
-      </NavItem>{" "}
-      <NavItem>
-        {" "}
-        <Link href="#">Link</Link>{" "}
-      </NavItem>{" "}
-      <NavItem>
-        {" "}
-        <Link href="#">Link</Link>{" "}
-      </NavItem>{" "}
-    </Nav>
+    <Router>
+     
+     <div>
+      
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="articles" element={<Articles />} />
+          <Route path="jobs" element={<Jobs />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="saved" element={<Saved />} />
+        </Routes>
+      </div>
+
+
+{/* Work on the router wit ui kit */}
+{/* Work on the router wit ui kit */}
+{/* Work on the router wit ui kit */}
+
+
+
+    </Router>
   );
 }
 
