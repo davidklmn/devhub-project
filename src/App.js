d95877315eeaf1882f49937828e85bf5b150ab7f
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-03-21 19:41:52 +0000

    Added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import './App.css';
-import Navigation from './components/Navigation';
-import JobSearch from './components/JobSearch.js';
-import Home from './components/pages/Home';
-import Articles from './components/pages/Articles';
-import ArticleSearch from './components/ArticleSearch';
-import { Container, Col, Row } from 'react-bootstrap';
+import "./App.css";
+import Navigation from "../src/components/Navigation";
+import JobSearch from "./components/JobSearch.js";
+import Home from "./components/Home";
+import Articles from "../src/components/ArticleSearch";
+import ArticleSearch from "./components/ArticleSearch";
+import { Container, Col, Row } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Container fluid>
-      <Row>
-        <Col xs={2} id="sidebar-wrapper">
-          <Navigation />
-        </Col>
-        <Col xs={8} id="page-content-wrapper">
-          <JobSearch></JobSearch>
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <Container fluid>
+        <Row>
+          <Col xs={2} id="sidebar-wrapper">
+            <Navigation />
+          </Col>
+          <Switch>
+            <Route path="/jobSearch">
+              <Col xs={8} id="page-content-wrapper">
+                <JobSearch></JobSearch>
+              </Col>
+            </Route>
+            <Route path="/articleSearch">
+              <Col xs={8} id="page-content-wrapper">
+                <ArticleSearch></ArticleSearch>
+              </Col>
+            </Route>
+          </Switch>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
