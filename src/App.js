40f7e781e00220c266b2d671e1d27872f3f1782e
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-03-22 09:58:02 +0000

    combined router with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,40 @@
+// import "./App.css";
+// import Navigation from "../src/components/Navigation";
+// import JobSearch from "./components/JobSearch.js";
+// import Home from "./components/Home";
+// import Articles from "../src/components/ArticleSearch";
+// import ArticleSearch from "./components/ArticleSearch";
+// import { Container, Col, Row } from "react-bootstrap";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Container fluid>
+//         <Row>
+//           <Col xs={2} id="sidebar-wrapper">
+//             <Navigation />
+//           </Col>
+//           <Switch>
+//             <Route path="/jobSearch">
+//               <Col xs={8} id="page-content-wrapper">
+//                 <JobSearch></JobSearch>
+//               </Col>
+//             </Route>
+//             <Route path="/articleSearch">
+//               <Col xs={8} id="page-content-wrapper">
+//                 <ArticleSearch></ArticleSearch>
+//               </Col>
+//             </Route>
+//           </Switch>
+//         </Row>
+//       </Container>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import Navigation from "../src/components/Navigation";
 import JobSearch from "./components/JobSearch.js";
@@ -7,23 +44,29 @@ import ArticleSearch from "./components/ArticleSearch";
 import { Container, Col, Row } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+
 function App() {
   return (
     <Router>
       <Container fluid>
         <Row>
           <Col xs={2} id="sidebar-wrapper">
-            <Navigation />
+            <Navigation/>
           </Col>
           <Switch>
+          <Route exact path="/home">
+              <Col xs={8} id="page-content-wrapper">
+                <Home/>
             <Route path="/jobSearch">
               <Col xs={8} id="page-content-wrapper">
-                <JobSearch></JobSearch>
+                <JobSearch/>
               </Col>
             </Route>
             <Route path="/articleSearch">
               <Col xs={8} id="page-content-wrapper">
-                <ArticleSearch></ArticleSearch>
+                <ArticleSearch />
+              </Col>
+            </Route>
               </Col>
             </Route>
           </Switch>
@@ -33,5 +76,5 @@ function App() {
   );
 }
 
-export default App;
 
+export default App;
