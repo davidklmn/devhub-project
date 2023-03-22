a839e51ed725021f241010410b8b133055582801
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-03-22 11:10:27 +0000

    fixed the bug with router so it calls the pages properly now

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
-
 import "./App.css";
 import Navigation from "../src/components/Navigation";
 import JobSearch from "./components/JobSearch.js";
 import Home from "./components/Home";
-import Articles from "../src/components/ArticleSearch";
+// import Articles from "../src/components/ArticleSearch";
 import ArticleSearch from "./components/ArticleSearch";
 import { Container, Col, Row } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
 function App() {
   return (
     <Router>
-      <Container fluid>
-        <Row>
-          <Col xs={2} id="sidebar-wrapper">
-            <Navigation/>
-          </Col>
-          <Switch>
-          <Route exact path="/home">
-              <Col xs={8} id="page-content-wrapper">
-                <Home/>
-            <Route path="/jobSearch">
-              <Col xs={8} id="page-content-wrapper">
-                <JobSearch/>
-              </Col>
-            </Route>
-            <Route path="/articleSearch">
-              <Col xs={8} id="page-content-wrapper">
-                <ArticleSearch />
-              </Col>
-            </Route>
-              </Col>
-            </Route>
-          </Switch>
-        </Row>
-      </Container>
+      <Navigation />
+      <Switch>
+      <Route exact path="/home">
+          <Home/>
+        </Route>
+        <Route path="/jobSearch">
+          <JobSearch/>
+        </Route>
+        <Route path="/articleSearch">
+          <ArticleSearch/>
+        </Route>
+      </Switch>
     </Router>
+ 
   );
 }
-
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,8 @@
-
 import React from "react";
-import Article from "../components/ArticleSearch";
+import Articles from "../components/ArticleSearch";
 import Home from "./Home";
 import Jobs from "../components/JobSearch";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
 
 import { BsFillHouseDoorFill } from "../../node_modules/react-icons/bs";
 import { HiNewspaper } from "../../node_modules/react-icons/hi";
@@ -12,9 +10,37 @@ import { MdWork } from "../../node_modules/react-icons/md";
 import { AiFillHeart } from "../../node_modules/react-icons/ai";
 import ReactDOM from "react-dom";
 
+// import { Nav } from "react-bootstrap";
 
-import { Nav } from "react-bootstrap";
+// function Navigation() {
+//   return (
+//     <div>
+//       <div>
+//         <p id="logo">
+//           <span>Dev</span>Hub
+//         </p>
+//       </div>
+//       <Nav className="flex-column">
+//         <Nav.Link as={Link} to="/home">
+//           Home
+//           <BsFillHouseDoorFill />
+//         </Nav.Link>
+//         <Nav.Link as={Link} to="/jobSearch">
+//           Jobs
+//           <MdWork />
+//         </Nav.Link>
+//         <Nav.Link as={Link} to="/articleSearch">
+//           Articles
+//           <HiNewspaper />
+//         </Nav.Link>
+//       </Nav>
+//     </div>
+//   );
+// }
 
+// export default Navigation;
+
+import Nav from "react-bootstrap/Nav";
 
 function Navigation() {
   return (
@@ -24,25 +50,24 @@ function Navigation() {
           <span>Dev</span>Hub
         </p>
       </div>
+
       <Nav className="flex-column">
         <Nav.Link as={Link} to="/home">
           Home
           <BsFillHouseDoorFill />
         </Nav.Link>
+
         <Nav.Link as={Link} to="/jobSearch">
           Jobs
           <MdWork />
         </Nav.Link>
+
         <Nav.Link as={Link} to="/articleSearch">
-          Articles
-          <HiNewspaper />
+          Articles <HiNewspaper />
         </Nav.Link>
       </Nav>
     </div>
   );
 }
 
-
 export default Navigation;
-
-
